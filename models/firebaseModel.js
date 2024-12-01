9501e899a4db57df9172868c0f46e224b54b5f43
kierpaolo <kupineda12@gmail.com>
2024-12-01 22:45:23 +0800

    Update firebaseModel.js

--- models/firebaseModel.js
+++ models/firebaseModel.js
@@ -1,27 +1,34 @@
-// models/firebaseModel.js
 const admin = require('firebase-admin');
 const serviceAccount = require('../firebase-config.json');
 
-// Initialize Firebase
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://current-sensor-forecast-default-rtdb.asia-southeast1.firebasedatabase.app/'  
-});
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://current-sensor-forecast-default-rtdb.asia-southeast1.firebasedatabase.app/'
+  });
+}
 
 const db = admin.database();
 
 module.exports = {
+  
   fetchRealtimeData: async () => {
-    const ref = db.ref('realtime');  
-    return new Promise((resolve, reject) => {
-      ref.once('value', snapshot => {
-        const data = snapshot.val();
-        if (data) {
-          resolve(data);
-        } else {
-          reject('No data found');
-        }
-      });
-    });
+    const ref = db.ref('realtime');
+    
+    try {
+      const snapshot = await ref.once('value');
+      const data = snapshot.val();
+
+      if (!data) {
+        throw new Error('No data found in the database.');
+      }
+
+      
+      return Array.isArray(data) ? data : Object.values(data);
+    } catch (error) {
+      console.error('Error fetching data from Firebase:', error.message);
+      throw new Error('Failed to fetch data from Firebase.');
+    }
   }
 };
